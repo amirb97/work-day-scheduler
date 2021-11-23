38cdbb751e8b586beb584d9d4ee9ebc9aa738340
Era <amirb2467@gmail.com>
2021-11-22 21:40:47 -0800

    each timeblock is editable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var tasks = {};
+
 // gets the current day in the format desired for header
 var momentDay = moment().format("dddd, MMMM Do");
 var momentHour = moment().hour();
@@ -20,12 +22,37 @@ function setTimeColors() {
             $("#" + i).find(".description").addClass("future");
         }
     };
-}
+};
+
+// task area was clicked
+$(".row").on("click", "span", function() {
+    // get current text and classes of span element
+    var text = $(this).text().trim();
+    var classes = $(this).attr('class');
+
+    // replace span element with a new textarea
+    var textInput = $("<textarea>").addClass(classes).val(text);
+    $(this).replaceWith(textInput);
+
+    // auto focus new element
+    textInput.trigger("focus");
+});
+
+// editable text field was un-focused
+$(".row").on("blur", "textarea", function() {
+    // gets current value of textarea
+    var text = $(this).val();
+    var index = $(this).parent().
+
+    $(".row").on("click", "button", function() {
+        console.log(text);
+    });
+});
 
 // initially sets the timeblock's colors
 setTimeColors();
 
-// interval that will reset the timeblock's colors every 5 minutes
+// interval that will reset the timeblock's colors every minute
 setInterval(function() {
     setTimeColors();
-}, 300000);
+}, 60000);
